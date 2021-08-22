d8f55c76c72b390fc2384d686b6d74dfa2266869
kuvaeva <email@gmail.ru>
2021-08-22 23:46:08 +0700

    create update

--- admin/catalog/catalogUpdate.php
+++ admin/catalog/catalogUpdate.php
@@ -2,8 +2,8 @@
 require_once '../../controller/AdminAppController.php';
 $app=New Admin();
 // $app->send_data();
-$app->all_categories();
-
+$params=$app->all_categories('products',$_GET['id']);
+$app->update();
 ?>
 
 
@@ -19,36 +19,46 @@ $app->all_categories();
     <form method='POST' enctype='multipart/form-data'> <!--php прочитать  файл -->
         <div>
             <label>Наименование товара </label>
-            <input type="text" name="product_name" >
+            <input type="text" name="product_name" value="<?php echo $params[0]['product_name'] ?>" >
+
+                      
         </div>
         <div>
             <label>Цена </label>
-            <input type="text" name="price" >
+            <input type="text" name="price" value="<?php echo $params[0]['price'] ?>" >
         </div>
         <div>
             <label>Изображение </label>
-            <input type="file" name="img" >
+            <input type="file" name="img" value="<?php echo $params[0]['img'] ?>" >
         </div>
         <div>
             <label>Описание </label>
-            <input type="text" name="text" >
+            <input type="text" name="text" value="<?php echo $params[0]['text'] ?>" >
         </div>
         <div>
             <label>Параметры </label>
-            <input type="text" name="params" >
+            <input type="text" name="params" value="<?php echo $params[0]['params'] ?>">
         </div>
         <div>
             <label>Категория </label>
-            <select type="text" name="categori"  >
-                <!-- <option value="1">Ветро-гидро защита</option> -->
-                <?php foreach( $app->all_categories() as $i):  ?>
-                   
-                <option value="<?php print($i['id']) ; ?>"><?php print($i['categori_name']) ; ?></option>
+            <select type="text" name="categori">
+                <option value="<?php print($app->all_categories('categories')[0]['id']) ; ?>"><?php print($app->all_categories('categories')[$params[0]['categori'] - 1]['categori_name']) ; ?></option>
+
+                <!-- Дублирование категории - убрать  -->
+                <?php foreach( $app->all_categories('categories') as $i):  ?>
+                    
+                   <?php if ($i['id'] !== $params[0]['categori']): ?>
+                        <option value="<?php print($i['id']) ; ?>"><?php print($i['categori_name']) ; ?></option>
+                    <?php endif; ?>
                 <?php endforeach; ?>
 
             </select>
         </div>
-        <button type='submit'>Отправить форму</button>
+
+        <div>
+        <input type="submit" class="btn btn-danger" value="Отправить">
+        </div>
+        
 
 
 
--- controller/AdminAppController.php
+++ controller/AdminAppController.php
@@ -5,6 +5,16 @@ ini_set('display_startup_errors', 1);
 ini_set('display_errors', '1');
 
 class Admin{
+
+    function file_save(){
+        if($_FILES)
+        {
+            $path='../../media/'.$_FILES['img']['name'];
+            move_uploaded_file($_FILES['img']['tmp_name'],$path);
+            return $path;
+        }
+    }
+
     function send_data(){
         if($_POST)
         {
@@ -18,14 +28,8 @@ class Admin{
             $text=$_POST['text'];
             $categori=(integer)$_POST['categori'];
 
-            if($_FILES)
-            {
-                $path='../../media/'.$_FILES['img']['name'];
-               // $file=$pach.basename($_FILES['img']['name']);
-                //$getFile=getimagesize($_FILES['img']['tmp_name']);
-                move_uploaded_file($_FILES['img']['tmp_name'],$path);
+            $path = $this->file_save();//сохрание пути к файлу изображения
 
-            }
             if($_POST['params'])
             {
                 $params=(float)$_POST['params'];
@@ -51,21 +55,144 @@ class Admin{
 
         }
     }
-        
-    function all_categories($p){ //
+    
+      
+    function all_categories($p,$param=null){
         $connect = new PDO(
             'mysql:host=localhost;dbname=std_54',
              'root', 
              '071452rdf'
           );
         //   print_r($connect->query("SELECT * FROM categories")->fetchAll(PDO::FETCH_ASSOC));
-        return $connect->query("SELECT * FROM {$p}")->fetchAll(PDO::FETCH_ASSOC);//вызываем тригер который возвращает все значения без передачи параметров
+        if($param==null){
+             return $connect->query("SELECT * FROM {$p}")->fetchAll(PDO::FETCH_ASSOC);//вызываем тригер который возвращает все значения без передачи параметров
     
+        }
+        else {
+            $data=$connect->prepare("SELECT * FROM {$p} WHERE id=?");
+            $data->execute([$param]);
+            return $data->fetchAll();
+        }
+       
         
 
     }
 
     
+
+
+    function update(){  //обновление и сравнение данных
+        if($_GET){
+           if($_POST){
+               $product_name=$_POST['product_name'];
+               $price=$_POST['price'];
+               $img=$_FILES['img'];
+               print_r($img);
+               $text=$_POST['text'];
+               $params=(float)$_POST['params'];
+               $categori = (integer)$_POST['categori'];
+
+               $data=$this->all_categories('products',$_GET['id']);
+
+            $connect = new PDO(
+            'mysql:host=localhost;dbname=std_54',
+                'root', 
+                '071452rdf'
+            );
+            $data=$connect->prepare("SELECT * FROM products WHERE id=?");
+            $data->execute([$_GET['id']]);
+            // Переопределение старой переменной
+            $data = $data->fetchAll();
+
+            if ($product_name === $data[0]['product_name']){
+                // Пропускаем
+            } else{
+                $query = $connect->prepare("UPDATE products SET product_name=:product_name WHERE id=:id");
+                $query->execute(
+                    [
+                        "id" => $_GET['id'],
+                        "product_name" => $product_name
+                    ]
+                );
+            }
+            if ($price === $data[0]['price']){
+                // ...
+            } else{
+                $query = $connect->prepare("UPDATE products SET price=:price WHERE id=:id");
+                $query->execute(
+                    [
+                        "id" => $_GET['id'],
+                        "price" => $price
+                    ]
+                );
+            }
+            if ($text === $data[0]['text']){
+                // ...
+            } else{
+                $query = $connect->prepare("UPDATE products SET text=:text WHERE id=:id");
+                $query->execute(
+                    [
+                        "id" => $_GET['id'],
+                        "text" => $text
+                    ]
+                );
+            }
+            // print($data[0]['params']);
+            //     print($params);
+            if ($params === $data[0]['params']){
+                // ...
+            } else {
+                $query = $connect->prepare("UPDATE products SET params=:params WHERE id=:id");
+                $query->execute(
+                    [
+                        "id" => $_GET['id'],
+                        "params" => $params
+                    ]
+                );
+            }
+
+            if ($categori=== $data[0]['categori']){
+                // ...
+            } else {
+                $query = $connect->prepare("UPDATE products SET categori=:categori WHERE id=:id");
+                $query->execute(
+                    [
+                        "id" => $_GET['id'],
+                        "categori" => $categori
+                    ]
+                );
+            }
+            
+           }
+
+        // if ($img === $data[0]['img']){
+        //     // ...
+        // } else {
+        //     $query = $connect->prepare("UPDATE products SET categori=:categori WHERE id=:id");
+        //     $query->execute(
+        //         [
+        //             "id" => $_GET['id'],
+        //             "categori" => $categori
+        //         ]
+        //     );
+        // }
+
+        if ($_FILES){
+            $img = $this->file_save();
+
+            $query = $connect->prepare("UPDATE products SET img=:img WHERE id=:id");
+            $query->execute(
+                [
+                    "id" => $_GET['id'],
+                    "img" => $img
+                ]
+            );
+        }
+        
+       }
+
+        
+    }
     function new_categori(){
         if($_POST)
         {
